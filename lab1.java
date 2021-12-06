680f053190533a8112959282e35533b6786a482f
Eugene <dedprod.2020@gmail.com>
2021-12-06 22:05:23 +0300

    1

--- lab1.java
+++ lab1.java
@@ -1,39 +1,19 @@
-import java.util.InputMismatchException;
+import java.util.regex.*;
 import java.util.Scanner;
+
 public class lab1 {
-    public static void main(String[] args) {
-        int a = -1;
-        int s =0;
-        int flag=0;
-        int fac=1;
-        int fac1=1;
-        try {
-            Scanner console = new Scanner(System.in);
-            System.out.println("Введите значение от 1 до 15: ");
-            a = console.nextInt();
-            console.close();
-        }
-        catch (InputMismatchException e) {
-            System.out.println("Error");
-            flag=1;
-        }
-        if ((flag==0) & (a>=0) & (a<+15)) {
-            s=a;
-            for(s=1; s<=a; s++) {
-                fac= (fac*s);
-            }
-            System.out.println("Значение посчитанное циклом For");
-            System.out.println(fac);
-            s=a;
-            while (s>=1){
-                fac1=(fac1*s);
-                s=s-1;
-            }
-            System.out.println("Значение посчитанное циклом While");
-            System.out.print(fac1);
-        }
-        else{
-            System.out.println("Введите другое значение");
+    public static void main(String args[]) {
+        String pattern = "(0[1-9]|[12]\\d|3[01])/(0[1-9]|1[012])/(19|[2-9][0-9])\\d\\d";
+        Pattern p = Pattern.compile(pattern);
+        Scanner data = new Scanner(System.in);
+        System.out.println("Введите дату");
+        String text = data.next();
+        data.close();
+        Matcher m = p.matcher(text);
+        if (m.matches() == true) {
+            System.out.println("Данная строка является датой типа dd/mm/yyyy и соответствует заданному диапазону");
+        } else {
+            System.out.println("Данная строка НЕ является датой типа dd/mm/yyyy и НЕ соответствует заднному диапазону");
         }
     }
 }
